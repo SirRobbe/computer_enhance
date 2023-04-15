aaa63708380d6ffd13a6cfc6596e12f4568d40f1
Fabian Behrens <behrens-fabian@gmx.de>
2023-04-15 20:37:12 +0200

    Added some docs to the header

--- perfaware/sirrobbe/cpp-sim86/sim86.h
+++ perfaware/sirrobbe/cpp-sim86/sim86.h
@@ -2,9 +2,13 @@
 
 #include <cstdint>
 
+// NOTE(Fabi):
+// Basic typedefs for the standardized sized integers for shorter
+// more readable names.
 typedef uint8_t u8;
 typedef int32_t i32;
 
+// List of all op codes that are supported by the simulation
 enum struct OpCode
 {
   MoveRegisterOrMemoryToOrFromRegister,
@@ -46,17 +50,30 @@ enum struct OpCode
   Count = 0,
 };
 
+// Binary definition of a cpu instruction
 struct InstructionLiteral
 {
+  // The op code that maps to the binary definition
   OpCode Code = OpCode::Count;
+
+  // The binary code which encodes the instruction
   u8 Sequence = 0;
+
+  // The Length of the binary code. This is important because the bit pattern
+  // of all instructions is not of the same size.
   i32 Length = 0;
 };
 
+// Returns the mnemonic for any op code that is related to a jump instruction
 const char* JumpOpCodeToMnemonic(OpCode code);
+
+// Reads one ore two bytes from the specified position of the program
 i32 ReadValue(u8* memory, i32* index, u8 w);
+
+// Returns the name of the register or the memory address specified by the reg field
 const char* GetRegisterOrMemory(u8 mod, u8 rm, u8 w, u8* memory, i32* index, char* buffer);
 
+// Look up table for all register names
 const char* regTable[][2] = {
     {"al", "ax"},
     {"cl", "cx"},
@@ -68,6 +85,7 @@ const char* regTable[][2] = {
     {"bh", "di"},
 };
 
+// Look up table for all effective address calculations
 const char* effectiveAddressCalculationTable[] = {
     "bx + si",
     "bx + di",
@@ -78,5 +96,3 @@ const char* effectiveAddressCalculationTable[] = {
     "bp",
     "bx",
 };
-
-InstructionLiteral test = {OpCode::AccumulatorToMemory, 0b11, 2};
